c728bf6d260e410b5d4a4ceede4313feb5030a4d
yatharth rawat <78906468+yatharthrawat0592@users.noreply.github.com>
2023-09-30 01:30:47 +0530

    Update wsConfig.js

--- ccsWS/webSocketServer/config/wsConfig.js
+++ ccsWS/webSocketServer/config/wsConfig.js
@@ -1,5 +1,5 @@
 module.exports = {
-    baseURL: "https://localhost:8800/api",
+    baseURL: "https://portal.amtech.technology:8800/api",
     requestTimeout: 5000,
     username: "amtech_admin@gmail.com",
     password: "amtech_admin@2023",
